047aba267b9b339787a06c572fb081033e0c32c9
Alex <feuerpers@gmail.com>
2021-10-31 21:47:30 +0200

    task
    task

--- homework_1.rb
+++ homework_1.rb
@@ -1,45 +1,31 @@
+# 1
+# Написати скрипт в який, використовуючи метод gets, потрібно передати з консолі назву тварини.
+# Після цього, вивести в консоль наступний текст I like _animal_, де замість _animal_ повинно бути значення отримане з консолі
 
-p "first task"
-print "Please, enter your favourite animal "
-animal = gets
-puts "I like ", animal
+# 2
+# Написати скрипт в який, використовуючи метод gets, потрібно передати з консолі ваш вік.
+# Після цього, вивести в консоль, через скількі років вам буде 100 (Текст можете придумати самі)
 
-p "second task"
-print "Enter your age "
-age = gets.to_i
-age = age + 100
-print "In 100 years you'll be ", age 
+# 3
+# Написати скрипт в який, використовуючи метод gets, потрібно передати з консолі список з трьох хоббі та записати їх в змінну
+# Приклад вводу: 
+# 'football, programming, hiking'
+# Після цього, потрібно обрати випадкове хоббі та вивести в консоль наступний текст Tell me something about _random_hobby_
+# де замість _random_hobby_ повинно бути випадкове хоббі
 
 
-p "third task"
-puts "Please enter at least 3 hobbies"
-print "first hobby: "
-a = gets.chomp
-print "second hobby: "
-b = gets.chomp
-print "third hobby: "
-c = gets.chomp
-arr = [a,b,c]
-puts "Tell me something about " + arr.sample
+# 4*
+# Дано дві змінні:
+# variable_1 = 1
+# variable_2 = 2
+# Потрібно щоб змінні variable_1 та variable_2 обмінялися своїми значеннями без використання третьої змінної
 
-p "fourth task"
-variable_1 = 1
-variable_2 = 2
-arr = [variable_1, variable_2]
-variable_1 = arr[1]
-variable_2 = arr[0]
-print "variable_1 = ", variable_1, " variable_2 = ", variable_2
-
-p "fifth task"
-hash = {
-1    => 2,
-nil  => 3,
-'kEy' => :value_1,
-:key  => :value_2,
-Key:    :value_3
- }
-
- puts hash.count { |k, _| k.to_s.downcase.include?('key') }
- 
- 
-
+# 5**
+# Написати скрипт який порахує кількість ключив хеша з назвою key без урахування його типу та реєстру, та виведе цю інформацію в консоль
+# hash = {
+#   1    => 2,
+#   nil  => 3,
+#  'kEy' => :value_1,
+#  :key  => :value_2,
+#   Key:    :value_3
+# }
