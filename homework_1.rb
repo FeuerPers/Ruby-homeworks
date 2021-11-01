5eedf99607c44f39aab858f09bbb018779bcf987
FeuerPers <feuerpers@gmail.com>
2021-11-02 00:35:43 +0200

    Updates in homework

--- homework_1.rb
+++ homework_1.rb
@@ -2,31 +2,43 @@
 p "first task"
 print "Please, enter your favourite animal "
 animal = gets
-puts "I like ", animal
+puts "I like #{animal}"
+
 
 p "second task"
 print "Enter your age "
 age = gets.to_i
-age = age + 100
-print "In 100 years you'll be ", age 
-
+age = 100 - age
+puts "In #{age} years you'll be 100" 
 
 p "third task"
-puts "Please enter at least 3 hobbies"
-print "first hobby: "
-a = gets.chomp
-print "second hobby: "
-b = gets.chomp
-print "third hobby: "
-c = gets.chomp
-arr = [a,b,c]
-puts "Tell me something about " + arr.sample
+puts "Please enter 3 hobbies"
+
+arr = gets.chomp
+b = arr.split(/,/)
+c = b.shuffle.first
+
+puts "Tell me something about #{c}"
+
 
 p "fourth task"
 variable_1 = 1
 variable_2 = 2
-arr = [variable_1, variable_2]
-variable_1 = arr[1]
-variable_2 = arr[0]
-print "variable_1 = ", variable_1, " variable_2 = ", variable_2
+variable_1, variable_2 = 2, 1
 
+puts variable_1, variable_2
+
+<<<<<<< Updated upstream
+=======
+p "fifth task"
+hash = {
+1    => 2,
+nil  => 3,
+'kEy' => :value_1,
+:key  => :value_2,
+Key:    :value_3
+ }
+
+ puts hash.count { |k, _| k.to_s.downcase.include? "key" }
+ 
+>>>>>>> Stashed changes
